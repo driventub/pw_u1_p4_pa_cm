55fbc3d398990c3cf7b553de48b63ab1b4067372
PC AS <aldazpaulo@gmail.com>
2023-05-24 19:49:36 -0500

    metodo remove

--- js/calculadora.js
+++ js/calculadora.js
@@ -22,7 +22,7 @@ function parsear(id) {
   }
 
   function eliminarElemento(){
-    document.getElementById('idEliminar').remove;
+    document.getElementById('idEliminar').remove();
   }
 
   function insertarElemento(){
